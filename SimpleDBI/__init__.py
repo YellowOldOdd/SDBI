7af33e28d0dab4da2d156ac4a7ad931f1b52334a
齐御 <yaoshi@xiaohongshu.com>
2021-04-13 14:07:39 +0800

    update log file

--- SimpleDBI/__init__.py
+++ SimpleDBI/__init__.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import logging
-
-format_str = '%(filename)s[line:%(lineno)d] - %(levelname)s : '
-format_str += '%(message)s' 
-logging.basicConfig(format=format_str, level=logging.INFO, )
+import sys
 
 EXIT_SIG = -1
+
+format_str = '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s : '
+format_str += '%(message)s'
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler(
+    'SDBI.log', mode='a', encoding="utf-8", delay=False)
+
+logging.basicConfig(format=format_str, level=logging.INFO, 
+    handlers=[file_handler, stdout_handler])
