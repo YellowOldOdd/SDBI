ac8f064deab5e451b20068bdb93a339248f056cd
齐御 <yaoshi@xiaohongshu.com>
2021-04-15 16:52:07 +0800

    rm default logger setting

--- SimpleDBI/__init__.py
+++ SimpleDBI/__init__.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python
 
-import logging
-import sys
-
 EXIT_SIG = -1
 
-format_str = '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s : '
-format_str += '%(message)s'
-
-stdout_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler(
-    'SDBI.log', mode='a', encoding="utf-8", delay=False)
-
-logging.basicConfig(format=format_str, level=logging.INFO, 
-    handlers=[file_handler, stdout_handler])
